e00e50a85943e54a5b289b9bec637e3e09f5a60d
Sai Hein Thu Ya Soe <135956428+saiheinthuyasoe@users.noreply.github.com>
2026-03-31 22:06:50 +0700

    Disable Vercel Image Optimize

--- clothing-store/next.config.ts
+++ clothing-store/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true, // disable Vercel image optimization
+
     remotePatterns: [
       {
         protocol: "https",
